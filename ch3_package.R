c396e88e8adfc87ee51136b5325e4ccb9ac77186
Kim-chowon <76463908+Kim-chowon@users.noreply.github.com>
2021-01-04 12:56:59 +0900

    delete files

--- ch3_package.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-data(iris)
-scatter <-  ggplot(data=iris, aes(x=Sepal.Length,
-                                  y=Sepal.Width)
-                   )
-scatter + geom_point(aes(color=Species,
-                         shape=Species)) +
-  xlab("Sepal Length") + ylab("Sepal Width") +
-  ggtitle("Sepal Length-Width")
