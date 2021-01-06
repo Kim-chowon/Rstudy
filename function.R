1ff868486769d6f1fe82801997370211e5480354
Kim-chowon <76463908+Kim-chowon@users.noreply.github.com>
2021-01-06 14:59:15 +0900

    20210106

---
+++ function.R
@@ -0,0 +1,103 @@
+# 야드를 미터로 변환하는 함수를 만들어 보겠습니다
+transLength <- function(x){
+  tlength <- round(x*0.9144, digits = 1) # 소수첫째자리까지 반올림
+  result <- paste(tlength, "m", sep="")
+  return(result) # 명령어 return: 정의된 함수의 실행 결과를 함수 내에서 작업공간으로 보내는 역할
+}
+
+y <- c(100,150,200)
+transLength(y)
+
+
+# return이 없을 경우 값을 확인하려면 print()
+transLength <- function(x){
+  tlength <- round(x*0.9144, digits = 1) # 소수첫째자리까지 반올림
+  result <- paste(tlength, "m", sep="")
+}
+transLength(100)
+print(transLength(100))
+
+#함수를 다른 함수에 넣을 땐 ()붙이면 안돼
+trans2 <- transLength  
+
+# 중간에 함수를 벗어나고 싶을 때 return명령어 쓴다
+# 숫자가 아닌 문자가 들어오면 함수를 종료하는 함수를 만들어 보겠다
+transLength <- function(x){
+  if(!is.numeric(x)) return("Not a Number")
+  tlength <- round(x*0.9144, digits = 1)
+  result <- paste(tlength, "m", sep="")
+  return(result) 
+}
+
+transLength("A")
+
+#### 함수인수의 특징 ####
+# 함수 인수에 Default값 정의할 수 있다.
+# 함수 인수는 이름을 가져야 한다.
+# 함수 인수 이름을 몰라도 순서를 알면 쓸 수 있다.
+# 가변적인 인수 리스트를 지정할 수 있다.
+
+# 멀티 인수
+transLength <- function(x, mult, unit){
+  tlength <- round(x*mult, digits = 1)
+  result <- paste(tlength, unit, sep="")
+  return(result) 
+}
+
+transLength(y, mult=3, unit="ft")
+transLength(y)
+
+# 초기값 지정
+transLength <- function(x, mult=0.9144, unit="m"){
+  tlength <- round(x*mult, digits = 1)
+  result <- paste(tlength, unit, sep="")
+  return(result) 
+}
+
+transLength(y)
+
+# digit인수 대신 ...
+transLength <- function(x, mult=0.9144, unit="m", ...){
+  tlength <- round(x*mult, ...)
+  result <- paste(tlength, unit, sep="")
+  return(result) 
+}
+
+transLength(y, digits=2)
+transLength(y)
+# round()이 초기값이 digits=0이기 때문에 정수로 출력된다. 
+# 다른 함수에서 사용하는 인수 이름을 사용하는 게 좋습니다.
+
+# 함수 자체가 인수가 될 수 있따.
+# FUN 인수
+transLength <- function(x, mult=0.9144, unit="m", FUN=round, ...){
+  tlength <- FUN(x*mult, ...)
+  result <- paste(tlength, unit, sep="")
+  return(result) 
+}
+
+# 어제 했던
+outer(asian.countries, info, FUN=paste, sep="-")
+
+# 함수 내의 인수는 함수가 종료되면 작업공간에서 사라진다
+# 함수는 일시적으로 생성한 로컬환경에서 작업하고, 로컬환경은 함수 종료와 동시에 소멸
+# 로컬환경은 현재의 작업환경인 글로벌 환경 안에 포함
+
+ls()
+
+#### 로컬환경과 글로벌환경 ####
+x <- 11:15
+scopetest <- function(x) {
+  cat("This is x: ", x, "\n")
+  rm(x) #x를 작업공간에서 제거했음
+  cat("This is x after removing x", x, "\n")
+}
+
+scopetest(x=15:11)
+# 로컬환경에서 인수를 먼저 찾는다.
+# 15:11 x가 정상적으로 출력된 후, x를 작업공간에서 제거하자, 로컬환경의 x인수는 더이상 없다. 
+# 로컬환경에 인수가 없으니 글로벌 환경에서 찾는다. 그러면 11:15가 출력된다. 
+rm(list=ls())
+
+# 함수 안의 인수를 먼저 찾고 함수 밖에서 인수를 찾는다는 것
+# 인수로 지정된 변수를 구분할 수 있도록 하는 것이 중요
---
+++ subset.R
@@ -0,0 +1,121 @@
+# 부분집합
+# 데이터 전처리
+
+#### 인덱싱 이용 ####
+
+$ # 데이터셋의 하나의 원소 추출
+  # 데이터 프레임에 적용하면 벡터 반환
+  # 리스트에 적용하면 원소 반환
+
+[[]] # 하나의 원소 추출하여 백터 형식으로 반환
+
+[] # 데이터셋으로부터 여러 개의 원소를 리스트/데이터프레임 형식으로 반환
+
+# 리스트 인덱싱
+str(mtcars)
+mtcars$mpg
+mtcars[["mpg"]]
+mtcars["mpg"]
+mtcars[c(1,4)]
+mtcars[-c(2:10)] #음과 양 한번에 사용 못함
+
+# 행렬 인덱싱
+str(iris)
+iris[1:5, ] # 처음 다섯개의 행 데이터프레임 추출
+iris[, c("Sepal.Length", "Sepal.Width")]
+iris[, "Sepal.Length"] # 열을 하나만 추출할 경우에 벡터형식으로 나온다
+iris[, "Sepal.Length", drop=F] # 원래 구조 유지
+iris["Sepal.Length"] # 원래 구조 유지하고 싶으면 리스트인덱싱이 낫다
+
+iris[1:5, c("Sepal.Length", "Sepal.Width")]
+
+# 조건 충족하는 것들만 인덱싱
+iris[iris$Sepal.Length > 7, ]
+iris[iris$Sepal.Length > 7, c("Sepal.Width", "Species")]
+
+#### 함수 이용 ####
+subset(iris,
+       subset = (Sepal.Length > 7), # 출력할 행의 조건
+       select = c("Sepal.Width", "Species"),# 출력할 열 지정
+)
+
+#### 랜덤 샘플 추출 #### 
+sample(x=1:10, size=5) # 비복원추출
+sample(1:10, 5, replace = T) # 복원추출
+
+sample(10) # 무작위 순열
+
+# 랜덤샘플을 동일하게
+# 난수생성 초기값 지정하면 된다
+set.seed(1)
+sample(1:10, 5, replace = T)
+
+sample(1:10, 5, replace = T)
+
+set.seed(1)
+sample(1:10, 5, replace = T)
+
+# 데이터 프레임으로부터 무작위 표본을 추출하려고 한다 
+# 행으로부터 무작위 표본을 얻고자 함
+# 그냥 sample함수는 열로부터 무작위 표본이 추출됨
+sample(iris,3)
+# 5개 열 중 3개의 열이 무작위로 추출
+set.seed(2)
+# 행번호로 구성된 무작위 서브셋 먼저 만들고
+# 이를 인덱스로 이용하여 추출
+index <- sample(nrow(iris), 3) # 아이리스의 행의 개수를 대상으로 3개의 행을 무작위 추출
+index
+iris[index, ]
+
+#### 중복값 제거한 서브셋 ####
+duplicated() # 중복 여부를 논리값으로 반환
+duplicated(c(1,1,2,2,3,3,4,5,6,7,1)) # 처음출현 = F, 중복되면 T
+# R은 각 행을 하나의 값으로 취급한다. 따라서 중복값=중복행
+id <- c("A", "B", "C")
+name <- c("Mouse", "Keyboard", "USB")
+price <- c(30000, 40000, 35000)
+product <- data.frame(id=id, name=name, price=price)
+product
+product <- rbind(product, c("C", "USB", "35000"))
+product
+
+duplicated(product) 
+product[!duplicated(product), ] # 중복 안된 행만 추출
+
+# 중복된 위치의 인덱스를 먼저 알고 제거하고 싶을 때
+which(duplicated(product))
+product[-which(duplicated(product)), ]
+index <- which(duplicated(product)) # 예쁘게
+product[-index, ]
+
+# 중복 원소 파악할 필요는 없을 때
+unique(product)
+
+
+#### 결측값 제거한 서브셋 ####
+# 결측값 포함된 데이더프레임으로부터 결측값 포함 행을 제거
+str(airquality)
+complete.cases(airquality) # 행에 결측값 하나라도 포함되어 있으면 F
+airquality.nona <- airquality[complete.cases(airquality),]
+airquality.nona
+
+airquality.nona <- na.omit(airquality)
+str(airquality.nona)
+
+#### 범주화 ####
+cut(x=범주화하고자하는 숫자벡터, breaks=벡터나 정수로 구간을 지정)
+cut(x=iris$Sepal.Width, breaks=c(0,1,2,3,4,5))
+# Levels: (0,1] (1,2] (2,3] (3,4] (4,5]
+cut(x=iris$Sepal.Width, breaks=c(5))
+# 임의로 만든 동일한 간격의 5개 구간
+
+# 구간 별 빈도 계산
+table(), summary()
+iris.cut <- cut(x=iris$Sepal.Width, breaks=c(5))
+table(iris.cut)
+summary(iris.cut)
+
+cut(x=iris$Sepal.Width, 
+    breaks=c(0,1,2,3,4,5),
+    labels=c("Smaller", "Small", "Medium", "Big", "Bigger"))
+table(iris.cut)
